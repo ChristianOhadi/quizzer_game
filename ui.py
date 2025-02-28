77d5eb4f3c932363db910eca65e18e0a616a1f73
ChristianOhadi <72025323+ChristianOhadi@users.noreply.github.com>
2025-02-28 23:38:28 +0000

    Add files via upload

---
+++ ui.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+THEME_COLOR = "#375362"
+
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzer")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text=0, bg="white")
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            text="some Question text",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic")
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2)
+
+        self.window.mainloop()
